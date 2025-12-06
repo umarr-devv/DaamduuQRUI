429c730bd76f3b5c9e6a0e8bab66c99bb2c8055f
umarr <umarr.developer@gmail.com>
2025-12-06 23:30:25 +0600

    added CustomFilePicker widget

---
+++ lib/widgets/file_picker.dart
@@ -0,0 +1,43 @@
+import 'package:daamduuqr_ui/daamduuqr_ui.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+
+class CustomFilePicker extends StatelessWidget {
+  const CustomFilePicker({
+    super.key,
+    required this.onPick,
+    this.size = 140,
+    this.fileType = FileType.image,
+  });
+
+  final FileType fileType;
+  final double size;
+  final void Function(FilePickerResult) onPick;
+
+  Future pick() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: fileType,
+    );
+    if (result != null) {
+      onPick(result);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: pick,
+      child: Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(color: theme.custom.transparent),
+        child: FCard.raw(
+          child: Icon(Icons.add, color: theme.custom.mutedForeground, size: 32),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'animated_indicator.dart';
+export 'file_picker.dart';
 export 'image.dart';
 export 'page_indicator.dart';
